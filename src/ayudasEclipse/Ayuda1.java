1ffbe9ec2e7f8893abe82aa2e881d5d0e373bb0f
Cristina <cristinald42@educastur.es>
2026-03-04 19:29:00 +0100

    Primer commit

---
+++ src/ayudasEclipse/Ayuda1.java
@@ -0,0 +1,30 @@
+package ayudasEclipse; //Declaración del paquete
+
+/* Esta es un clase de prueba
+ * para aprender los conceptos básicos
+ * de programación
+ */
+
+public class Ayuda1 {
+
+	public static void main(String[] args) {
+		/* DEclaramos una variable
+		 * y la inicializamos
+		 */
+		int numero;
+		numero=25;
+		
+		/*Declaramos e inicializamos 
+		 * la variable en una única sentencia
+		 */
+		
+		int numero1=25;
+		
+		System.out.println(numero1);
+		
+		System.out.println(numero);
+	
+
+	}
+
+}
